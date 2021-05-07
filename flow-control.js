8d3412c3e0f2b552cc7629845a99a14bd5e8ef9e
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-07 23:06:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,51 +1,52 @@
-function basicTeenager(age){
-
-    if (age <= 19 && age >= 13){
-      return "You are a teenager!";
-    }
+function basicTeenager(age) {
+if (age >= 13 && age <= 19) {
+return "You are a teenager!";
+}
 }
 
-function teenager(age){
-
-  if (age <= 19 && age >= 13){
-    console.log("You are a teenager!");
-  }
-  else{
-    return "You are not a teenager";
-  }
+function teenager(age) {
+if (age >= 13 && age <= 19) {
+return "You are a teenager!";
+} else {return "You are not a teenager";}
 }
 
-function ageChecker(age){
-  //  Define a function ageChecker that takes in an age as a parameter.
-  //If the age is between 13-19 it should return "You are a teenager!".
-  //If the age is 12 or below, it should return "You are a kid".
-  //If the age is above 19, it should return "You are a grownup"
-
-function ageChecker(age){
-  if (age <= 19 && age => 13){
-    console.log("You are a teenager!");
-  }
-  else if(age < 13){
-    console.log("You are a kid");
-  }
-  else{
-    return "You are a grownup";
-  }
+function ageChecker(age) {
+if (age >= 13 && age <= 19) {
+return "You are a teenager!";
+} else if (age <= 12) {
+return "You are a kid";
+} else {return "You are a grownup";}
 }
 
-function ternaryTeenager(age){
-
-function ternaryTeenager(age){
-  age => 13 && age <= 19 ? return "You are a teenager": "You are not a teenager"
+function ternaryTeenager(age) {
+if (age >= 13 && age <= 19) {
+return "You are a teenager";
+} else {return "You are not a teenager";}
 }
 
-function switchAge(age){
+function switchAge(age) {
+switch(age) {
+case 13:
+return "You are a teenager"
 
-  switch (age)
-  case age <= 19 && age => 13:
-   console.log("You are a teenager");
-    break;
-  default:
-    return "You have an age";
-    break;
+case 14:
+return "You are a teenager"
+
+case 15:
+return "You are a teenager"
+
+case 16:
+return "You are a teenager"
+
+case 17:
+return "You are a teenager"
+
+case 18:
+return "You are a teenager"
+
+case 19:
+return "You are a teenager"
+default:
+return "You have an age"
+}
 }
