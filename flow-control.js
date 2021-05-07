1462689c8cc41263b5e7d8723029a3acc9923b65
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-07 23:18:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,20 +6,18 @@ function basicTeenager(age)
 
 function teenager(age)
 {
-  if(age >= 13 && age <= 19)
+if(age >= 13 && age <= 19)
 {return(`You are a teenager!`)}
 else {return (`You are not a teenager`)}
 
 }
 
-function ageChecker(age){
-
-  if(age >= 13 && age <= 19)
-  {return"You are a teenager";}
-  elseif(age<= 12)
-{return"you are a kid";
-}
-else {return "you are a grownup";}
+function ageChecker(age) {
+if (age >= 13 && age <= 19) {
+return "You are a teenager!";
+} else if (age <= 12) {
+return "You are a kid";
+} else {return "You are a grownup";}
 }
 
 function ternaryTeenager(age)
