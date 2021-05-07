d388f18ba4868b3be0e8365f18e6a872a6449e4b
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-07 21:24:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,58 @@
-function basicTeenager(age) {
+function basicTeenager(age)
+{
+  if(age >= 13 && age <= 19)
+  {
+  return(`You are a teenager!`)
+}
+}
+
+function teenager(age)
+{
+  if(age >= 13 && age <= 19)
+  {
+  return(`You are a teenager!`)
+}
+else {
+  return (`You are not a teenager`)
+
+}
+}
+
+function ageChecker(age)
+ {
+  if(age >= 13 && age <= 19)
+  {
+  return(`You are a teenager!`)
+{
+if (age <= 12)
+ {
+  return (`You are a kid`)
+
+}
+if (age >= 20)
+  return (`You are a grownup`)
+}
+
+
+function ternaryTeenager(age)
+ {
+return(`You are not a teenager`)
+
+  if (age >= 13 && age <= 19)
+  {
+  return(`You are a teenager`)
+}
 
 }
 
-function teenager(age) {
+function switchAge(age)
+ {
+   var age = 13 || 14 || 15 || 16 || 17|| 18 || 19
+   switch(age) {
+     case "teenager":
+      return ("You are a teenager")
+      default :
+      return "You have an age"
 
 }
-
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
 }
