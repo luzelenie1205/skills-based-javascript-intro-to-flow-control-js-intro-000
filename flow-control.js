832ba67cf5a152cfa67104c9dab46d7a520f34c4
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-07 22:05:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -20,13 +20,13 @@ else {
 
 function ageChecker(age)
  {
-  if(age <= 19 && age >= 13)
+  if(age >= 13 && age <= 19)
   {
   console.log(`You are a teenager!`)
 }
 else if (age < 13)
  {
-  console.log (`You are a kid`)
+  console.log "You are a kid")
 }
 else
 {
